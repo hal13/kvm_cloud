9268bc9cf61c5cda5bd2cfb150ef7dd55c30ac5e
vagrant <vagrant@localhost.localdomain>
2016-03-28 21:38:23 +0900

    add_devise_controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions'
+    registrations: 'users/registrations'
+    passwords:     'users/passwords'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
