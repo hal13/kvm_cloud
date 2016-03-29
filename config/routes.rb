422eb274dd8b242c9e96f0d7ff09b74934f6d2b4
vagrant <vagrant@localhost.localdomain>
2016-03-29 22:39:46 +0900

    update_root_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
+  root to: "home#index"
+
+  get 'home/show'
+
+  # resources :users
   # devise_for :users
-  devise_for :users, controllers: {
-    sessions:      'users/sessions'
-    registrations: 'users/registrations'
-    passwords:     'users/passwords'
+  # ログイン画面をホームにする
+  # devise_scope :user do
+    # root :to => "devise/sessions#new"
+  # end
+  devise_for :users, :controllers => {
+    :sessions      => 'users/sessions',
+    :registrations => 'users/registrations',
+    :passwords     => 'users/passwords'
   }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
