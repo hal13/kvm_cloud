01aee68aba56dcf345e3a0be8465feefeed4feb0
vagrant <vagrant@localhost.localdomain>
2016-04-01 21:55:58 +0900

    add_username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
   
-  validates :username, presence: true, uniqueness: true
+  # validates :username, presence: true, uniqueness: true
+  
+  # アクセサ
+  attr_accessor :login
+  
+  # 認証条件をオーバーライド
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+
 end
